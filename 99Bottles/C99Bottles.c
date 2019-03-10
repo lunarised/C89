6d606e85f4ecf4070e3d985e94faa2de19ee4a0b
lunarised <thunder.james.a@gmail.com>
2019-03-11 12:07:34 +1300

    Fixed Inconsistency in grammar

--- 99Bottles/C99Bottles.c
+++ 99Bottles/C99Bottles.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 int rMethod(int _bottle){
-	if(_bottle > 1){
+	if(_bottle > 2){
 		printf("%d bottles of beer on the wall!\n", _bottle);
 		printf("%d bottles of beer!\n", _bottle);
 		printf("You take one down, and pass it around!\n");
@@ -9,6 +9,13 @@ int rMethod(int _bottle){
 		printf("%d bottles of beer on the wall!\n", _bottle);
 		printf("\n");
 		rMethod(_bottle);
+	}
+	else if (_bottle == 2){	
+		printf("%d bottles of beer on the wall!\n", _bottle);
+		printf("%d bottles of beer!\n", _bottle);
+		printf("Take it down, and pass it around!\n");
+		_bottle--;
+		printf("%d more bottle of beer on the wall!\n", _bottle);
 	}
 	else if (_bottle == 1){	
 		printf("%d more bottle of beer on the wall!\n", _bottle);
